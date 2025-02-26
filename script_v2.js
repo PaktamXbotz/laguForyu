d47873afe4b6c62ac52c70ed2875054e3374655c
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2025-02-26 19:20:56 +0800

    Delete script_v2.js

--- script_v2.js
+++
@@ -1,4 +0,0 @@
-import './lyricsDisplay.js'; // Import the lyrics display functionality
-
-// Other existing code in script.js can remain here if needed
-// For example, any additional functionality that is not related to lyrics display
